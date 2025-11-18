15ff4f15acbb5d587f3f8c07785c67ce596e1319
Judarin15 <judarin1120@gmail.com>
2025-11-17 20:14:11 -0500

    Se agrego el link de la pagina principal en el logo de sistema en el apartado de Admin

--- resources/views/plantilla/menu.blade.php
+++ resources/views/plantilla/menu.blade.php
@@ -2,7 +2,7 @@
     <!--begin::Sidebar Brand-->
     <div class="sidebar-brand">
         <!--begin::Brand Link-->
-        <a href="../index.html" class="brand-link">
+        <a href="http://127.0.0.1:8000" class="brand-link">
             <!--begin::Brand Image-->
             <img src="{{asset('assets/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
                 class="brand-image opacity-75 shadow" />
